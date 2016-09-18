1d95281997f1f34768f1103f3d39e6dcb272ed06
pvgl <ab11xy@hughes.net>
2016-09-17 19:58:13 -0500

    ProgrammingAssignment2

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## omit repeated calculation by storing matrix
+
+## create shortcut by recalling matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+         inv <- NULL
+         set <- function(y)   {
+                 x <<- y
+                 inv <<- NULL
+                          }
+        get <- function() x
+        setInverse <-function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+                setInverse = setInverse,
+                getInverse = getInverse)
+         }
+
+
+## uses cache matrix to get inverse calculation from cache
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
+ }
